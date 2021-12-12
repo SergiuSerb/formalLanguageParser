dc864002cbb1a17accdd629c7f69a7ef709b0a70
Sergiu Serb <ovidiusergiu_7@yahoo.com>
2021-12-12 02:26:13 +0200

    Implemented UI.

--- FormalLanguageParser/Application/UserInterface/Windows/MainWindow.xaml.cs
+++ FormalLanguageParser/Application/UserInterface/Windows/MainWindow.xaml.cs
@@ -1,28 +1,120 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Application.Business.Models;
+using Application.Business.ParseLanguage;
 
 namespace Application
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void parseButton_Click(object sender, RoutedEventArgs e)
+        {
+            string textToParse = " ";
+            bool hasNoErrors = true;
+            if (tabControl.SelectedIndex == 0)
+            {
+                textToParse = textBoxInputTextBox.Text;
+            }
+            else
+            {
+                try
+                {
+                    textToParse = File.ReadAllText($"{fileInputTextBox.Text}");
+                }
+                catch (Exception exception)
+                {
+                    resultTextBox.Text = "Cannot read file. Please check if the path is correct and the file exists.";
+                    hasNoErrors = false;
+                }
+            }
+
+            if (hasNoErrors)
+            {
+                ParseLanguageRequest request = CreateParseLanguageRequest(textToParse);
+                ParseLanguageCommand command = CreateParseLanguageCommand();
+                ParseLanguageResponse response = command.Execute(request);
+
+                resultTextBox.Text = ParseResponse(response);
+            }
+        }
+
+        private string ParseResponse(ParseLanguageResponse response)
+        {
+            if (!response.IsValid)
+            {
+                return response.Reason;
+            }
+
+            string parsedInitialText = ParseInitialText(response);
+            string parsedProductions = ParseProductions(response);
+            string parsedTerminalSymbols = ParseTerminalSymbols(response);
+            string parsedNonTerminalSymbols = ParseNonTerminalSymbols(response);
+
+            return $"{parsedInitialText}\n{parsedProductions}\n{parsedNonTerminalSymbols}\n{parsedTerminalSymbols}";
+        }
+
+        private string ParseInitialText(ParseLanguageResponse response)
+        {
+            return $"Init = {response.InitialText}";
+        }
+
+        private string ParseProductions(ParseLanguageResponse response)
+        {
+            string parsedResponse = new string(' ', 0);
+            foreach (Production production in response.Productions)
+            {
+                parsedResponse += $"{production.Input}->{production.Output}; ";
+            }
+
+            return $"Prod = {{{parsedResponse}}}";
+        }
+
+        private string ParseNonTerminalSymbols(ParseLanguageResponse response)
+        {
+            string parsedResponse = new string(' ', 0);
+            foreach (string symbol in response.NonTerminalSymbols)
+            {
+                parsedResponse += symbol + ", ";
+            }
+
+            parsedResponse = parsedResponse.Remove(parsedResponse.Length - 2);
+
+            return $"Vn = {{{parsedResponse}}}";
+        }
+
+        private string ParseTerminalSymbols(ParseLanguageResponse response)
+        {
+            string parsedResponse = new string(' ', 0);
+            foreach (string symbol in response.TerminalSymbols)
+            {
+                parsedResponse += symbol + ", ";
+            }
+
+            parsedResponse = parsedResponse.Remove(parsedResponse.Length - 2);
+
+            return $"Vt = {{{parsedResponse}}}";
+        }
+
+        private ParseLanguageCommand CreateParseLanguageCommand()
+        {
+            return new ParseLanguageCommand();
+        }
+
+        private ParseLanguageRequest CreateParseLanguageRequest(string textToParse)
+        {
+            return new ParseLanguageRequest
+            {
+                TextToParse = textToParse
+            };
         }
     }
 }
